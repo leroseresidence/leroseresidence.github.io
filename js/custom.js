5f85d6391d2da73fcfb0e1620dafcd9f5b3ebe04
Donato Marrazzo <donato.marrazzo@gmail.com>
2025-12-26 16:27:20 +0100

    contact form email

--- js/custom.js
+++ js/custom.js
@@ -8,18 +8,25 @@ function tfuse_custom_form(){
 	jQuery("#send").bind("click", function(){
 		
 	my_error = false;
+	// Rimuovi messaggi di errore precedenti
+	jQuery(".ajax_form .error-message").remove();
+	
 	jQuery(".ajax_form input, .ajax_form textarea, .ajax_form radio, .ajax_form select").each(function(i)
 	{
 				var surrounding_element = jQuery(this);
-				var value               = jQuery(this).attr("value");
+				var value               = jQuery(this).val();
 				var check_for 			= jQuery(this).attr("id");
 				var required 			= jQuery(this).hasClass("required");
+
+				// Rimuovi messaggio di errore esistente per questo campo
+				surrounding_element.next(".error-message").remove();
 
 				if(check_for == "email"){
 					surrounding_element.removeClass("error valid");
 					baseclases = surrounding_element.attr("class");
 					if(!value.match(/^\w[\w|\.|\-]+@\w[\w|\.|\-]+\.[a-zA-Z]{2,4}$/)){
 						surrounding_element.attr("class",baseclases).addClass("error");
+						surrounding_element.after("<span class='error-message'>Inserire un indirizzo email valido</span>");
 						my_error = true;
 					}else{
 						surrounding_element.attr("class",baseclases).addClass("valid");
@@ -31,6 +38,7 @@ function tfuse_custom_form(){
 					baseclases = surrounding_element.attr("class");
 					if(value == "" || value == "Name"){
 						surrounding_element.attr("class",baseclases).addClass("error");
+						surrounding_element.after("<span class='error-message'>Il nome è obbligatorio</span>");
 						my_error = true;
 					}else{
 						surrounding_element.attr("class",baseclases).addClass("valid");
@@ -42,6 +50,7 @@ function tfuse_custom_form(){
 					baseclases = surrounding_element.attr("class");
 					if(value == "" || value == "Message"){
 						surrounding_element.attr("class",baseclases).addClass("error");
+						surrounding_element.after("<span class='error-message'>Il messaggio è obbligatorio</span>");
 						my_error = true;
 					}else{
 						surrounding_element.attr("class",baseclases).addClass("valid");
@@ -53,6 +62,7 @@ function tfuse_custom_form(){
 					baseclases = surrounding_element.attr("class");
 					if(value == ""){
 						surrounding_element.attr("class",baseclases).addClass("error");
+						surrounding_element.after("<span class='error-message'>Questo campo è obbligatorio</span>");
 						my_error = true;
 					}else{
 						surrounding_element.attr("class",baseclases).addClass("valid");
@@ -62,31 +72,31 @@ function tfuse_custom_form(){
 
 			   if(jQuery(".ajax_form input, .ajax_form textarea, .ajax_form radio, .ajax_form select").length  == i+1){
 					if(my_error == false){
-						jQuery(".ajax_form").slideUp(400);
-
-						var $datastring = "ajax=true";
-						jQuery(".ajax_form input, .ajax_form textarea, .ajax_form radio, .ajax_form select").each(function(i)
-						{
-							var $name = jQuery(this).attr('name');
-							var $value = encodeURIComponent(jQuery(this).attr('value'));
-							$datastring = $datastring + "&" + $name + "=" + $value;
-						});
-
-
-						jQuery(".ajax_form #send").fadeOut(100);
-
-						jQuery.ajax({
-						   type: "POST",
-						   url: "./sendmail.php",
-						   data: $datastring,
-						   success: function(response){
-						   jQuery(".ajax_form").before("<div class='ajaxresponse' style='display: none;'></div>");
-						   jQuery(".ajaxresponse").html(response).slideDown(400);
-						   jQuery(".ajax_form #send").fadeIn(400);
-						   jQuery(".ajax_form input, .ajax_form textarea, .ajax_form radio, .ajax_form select").val("");
-							   }
-							});
-						}
+						// Costruisci il mailto link
+						var recipientEmail = "info@leroseresidence.com";
+						var name = jQuery("#name").val();
+						var email = jQuery("#email").val();
+						var category = jQuery("#contact_select_1 option:selected").text();
+						var subject = jQuery("#subject").val();
+						var message = jQuery("#message").val();
+						
+						var fullSubject = "[" + category + "] " + subject;
+						var body = "Nome: " + name + "\n";
+						body += "Email: " + email + "\n";
+						body += "Categoria: " + category + "\n\n";
+						body += "Messaggio:\n" + message;
+						
+						var mailtoLink = "mailto:" + recipientEmail + 
+							"?subject=" + encodeURIComponent(fullSubject) + 
+							"&body=" + encodeURIComponent(body);
+						
+						// Apri il client email
+						window.location.href = mailtoLink;
+						
+						// Mostra messaggio di conferma
+						jQuery(".ajax_form").before("<div class='ajaxresponse' style='display: none;'></div>");
+						jQuery(".ajaxresponse").html("<p>Il tuo client email si è aperto. Invia l'email per completare la richiesta.</p>").slideDown(400);
+					}
 				}
 
 			});
